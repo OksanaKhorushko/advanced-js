96a67a66361a1f2214a8468cacfe5dcd3dd45cca
OksanaKhorushko <horushkosana@gmail.com>
2022-08-23 16:37:08 +0300

    Home work  2

--- js/script.js
+++ js/script.js
@@ -1,32 +1,82 @@
 'use strict'
 
-const headerButton = document.querySelector('.cart-button');
+// const headerButton = document.querySelector('.cart-button');
 
-const goods = [
-    { imgUrl: 'img/shirt.jpg', title: 'Shirt', price: 150 },
-    { imgUrl: 'img/socks.jpg', title: 'Socks', price: 50 },
-    { imgUrl: 'img/jacket.jpg', title: 'Jacket', price: 350 },
-    { imgUrl: 'img/shoes.jpg', title: 'Shoes', price: 250 },
-];
+class GoodsList {
+    constructor(container = '.goods-list') {
+        this.container = container;
+        this.goods = [];
+        this._fetchGoods();
+        this.render();
+    }
 
-const renderGoodsItem = ({
-    imgUrl = 'https://via.placeholder.com/1200x1600?text=No+image',
-    title = 'No title',
-    price = 0,
-}) => {
-    return (
-        `<div class="goods-item">
-            <img src="${imgUrl}" alt="${title}"></img>
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button>Добавить</button>
-        </div>`
-    );
-};
+    _fetchGoods() {
+        this.goods = [
+            { id: 1, title: 'Shirt', price: 150, imgUrl: 'img/shirt.jpg' },
+            { id: 2, title: 'Socks', price: 50, imgUrl: 'img/socks.jpg' },
+            { id: 3, title: 'Jacket', price: 350, imgUrl: 'img/jacket.jpg' },
+            { id: 4, title: 'Shoes', price: 250, imgUrl: 'img/shoes.jpg' },
+        ]
+    }
 
-const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+    getTotalPrice() {
+        return this.goods.reduce((acc, product) => acc + product.price, 0);
+    }
 }
 
-renderGoodsList(goods);
+class ProductItem {
+    constructor({
+        title = 'No title',
+        price = 0,
+        imgUrl = 'https://via.placeholder.com/1200x1600?text=No+image',
+        id,
+    }) {
+        this.title = title;
+        this.id = id;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    render() {
+        return  `<div class="goods-item">
+            <img src="${this.imgUrl}" alt="${this.title}"></img>
+            <h3>${this.title}</h3>
+            <p>${this.price}</p>
+            <button class = 'buy-btn'>Добавить</button>
+         </div>`
+    }
+}
+
+class Cart {
+    addProduct() {
+
+    }
+
+    removeProduct() {
+
+    }
+
+    changeProduct() {
+
+    }
+
+    render() {
+
+    }
+}
+
+class CartItem {
+    render() {
+
+    }
+}
+
+let list = new GoodsList();
